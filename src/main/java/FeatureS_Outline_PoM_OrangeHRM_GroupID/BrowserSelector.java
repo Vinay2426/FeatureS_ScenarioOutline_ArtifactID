d4344d334d4cb17d9f550535f3a1762ba4bfd5a0
vinay2426 <vinaybkahir@gmail.com>
2019-10-23 12:36:18 +0100

    commit

--- src/main/java/FeatureS_Outline_PoM_OrangeHRM_GroupID/BrowserSelector.java
+++ src/main/java/FeatureS_Outline_PoM_OrangeHRM_GroupID/BrowserSelector.java
@@ -132,31 +132,3 @@ public class BrowserSelector extends Utils
         }
     }
 }
-//    LoadProps loadProps = new LoadProps();
-//
-//    public void setUpBrowser()
-//    {
-//        //String browser = System.getProperty("browser");
-//        String browser = loadProps.getProperty("browser");
-//
-//        if (browser.equalsIgnoreCase("firefox"))
-//        {
-//            System.setProperty("webdriver.gecko.driver", "src\\test\\Resources\\BrowserDriver\\geckodriver.exe");
-//            driver = new FirefoxDriver();
-//        }
-//        else if (browser.equalsIgnoreCase("chrome"))
-//        {
-//            System.setProperty("webdriver.chrome.driver", "src\\test\\Resources\\BrowserDriver\\chromedriver.exe");
-//            driver = new ChromeDriver();
-//        }
-//        else if (browser.equalsIgnoreCase("ie"))
-//        {
-//            System.setProperty("webdriver.ie.driver", "src\\test\\Resources\\BrowserDriver\\IEDriverServer.exe");
-//            driver= new InternetExplorerDriver();
-//        }
-//        else
-//        {
-//            System.out.println("Browser name is empty or typed wrong : " + browser);
-//        }
-//    }
-
